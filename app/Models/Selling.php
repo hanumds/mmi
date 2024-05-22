d40eb13bc49b3ea81625bb0dcce3e916b6e8f373
sihaisyam <si.haisyam@gmail.com>
2024-05-22 17:44:03 +0700

    Pertemuan 10

--- app/Models/Selling.php
+++ app/Models/Selling.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Selling extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'cashier_id',
+        'date_sell',
+        'product_status',
+        'grand_total',
+    ];
+
+    public function details(){
+        return $this->hasMany(SellingDetail::class, 'id', 'id_sell');
+    }
 }
